21d2e6bbc57b51daa68a7ac8f120d2bd65c66082
Petru.Dimi <dimitriu_petru@yahoo.com>
2024-05-21 19:14:18 +0300

    added random generated number, dialog lines, and getPlayerGuess function

--- guess.js
+++ guess.js
@@ -68,40 +68,8 @@ const getPlayerGuess = () => {
   }
 };
 
-// const test = getPlayerGuess();
-// console.log(test);
-const checkGuess = (userNum, correctNum) => {
-  if (userNum < correctNum) {
-    return `Too low ${dialog.incorrectGuess}`;
-  }
-  if (userNum > correctNum) {
-    return `Too high ${dialog.incorrectGuess}`;
-  }
-  if (userNum === correctNum) {
-    return `${dialog.correctGuess}`;
-  }
-};
-
 alert(dialog.welcome);
 
-const game = () => {
-  const numberToBeGuessed = generateRandomNumber();
-  let counter;
+const test = getPlayerGuess();
+console.log(test);
 
-  for (counter = 9; counter >= 0; counter--) {
-    const userGuess = getPlayerGuess();
-    console.log(checkGuess(userGuess, numberToBeGuessed))
-    if(userGuess === numberToBeGuessed){
-      break
-    }else if(counter !== 0){
-      console.log(counter === 1 ? "You have 1 more guess." : `You have ${counter} guesses remaining.`);
-    } else{
-      console.log("You lost! And now your soul is mine!");
-      return
-    }
-  }
-
-  return `You did it using ${10-counter} attempts`
-};
-
-game()
